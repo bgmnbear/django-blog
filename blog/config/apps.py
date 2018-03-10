61a2b0da5c04301913191ab34facb5e679cdf235
unknown <jasonwhister@gmail.com>
2018-03-10 17:02:05 +0800

    Add config models

--- blog/config/apps.py
+++ blog/config/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class BlogModelConfig(AppConfig):
-    name = 'blog_model'
+class ConfigConfig(AppConfig):
+    name = 'config'
