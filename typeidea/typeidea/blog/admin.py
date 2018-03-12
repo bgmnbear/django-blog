1b96e1b2af5d9e342275797076251e66c11ff5d1
unknown <jasonwhister@gmail.com>
2018-03-12 20:55:31 +0800

    配置 admin 管理后台

--- typeidea/typeidea/blog/admin.py
+++ typeidea/typeidea/blog/admin.py
@@ -3,19 +3,24 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from typeidea.custom_site import custom_site
 from .models import Post, Category, Tag
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'category', 'status', 'owner', 'created_time']
+    list_filter = ['category', 'owner']
+    search_fields = ['title', 'category__name', 'owner__username']
+    save_on_top = True
+    show_full_result_count = True
 
 
-@admin.register(Category)
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     pass
