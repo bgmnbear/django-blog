7031a704d68eb2efe84a807f786e665b4e149de6
unknown <jasonwhister@gmail.com>
2018-03-15 16:50:45 +0800

    增加页面结构

--- typeidea/typeidea/blog/views.py
+++ typeidea/typeidea/blog/views.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def post_list(request, category_id=None, tag_id=None):
+    context = {
+        'name': 'post_list',
+    }
+    return render(request, 'blog/list.html', context=context)
+
+
+def post_detail(request, post_id=None):
+    context = {
+        'name': 'post_list',
+    }
+    return render(request, 'blog/detail.html', context=context)
--- typeidea/typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/typeidea/settings/base.py
@@ -4,7 +4,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -16,13 +15,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'blog',
     'config',
     'comment',
+    'typeidea',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -62,8 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'typeidea.wsgi.application'
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -82,7 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -95,7 +91,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
