162fbe92064354ce56958fe825e6dfabdc2058b8
unknown <jasonwhister@gmail.com>
2018-03-19 18:07:27 +0800

    function view 修改为 class based view

--- typeidea/typeidea/blog/views.py
+++ typeidea/typeidea/blog/views.py
@@ -1,84 +1,82 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.paginator import Paginator, EmptyPage
-from django.http import Http404
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post, Tag, Category
 from comment.models import Comment
 from config.models import SideBar
 
 
-def get_common_context():
-    categories = Category.objects.filter(status=1)
+class CommonMixin(object):
+    def get_context_data(self, **kwargs):
+        categories = Category.objects.filter(status=1)
 
-    nav_cates = []
-    cates = []
-    for cate in categories:
-        if cate.is_nav:
-            nav_cates.append(cate)
-        else:
-            cates.append(cate)
+        nav_cates = []
+        cates = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_cates.append(cate)
+            else:
+                cates.append(cate)
 
-    side_bars = SideBar.objects.filter(status=1)
+        side_bars = SideBar.objects.filter(status=1)
 
-    recently_posts = Post.objects.filter(status=1)[:10]
-    # hot_posts = Post.objects.filter(status=1).order_by('views')[:10]
-    recently_comments = Comment.objects.filter(status=1)[:10]
+        recently_posts = Post.objects.filter(status=1)[:10]
+        # hot_posts = Post.objects.filter(status=1).order_by('views')[:10]
+        recently_comments = Comment.objects.filter(status=1)[:10]
 
-    context = {
-        'nav_cates': nav_cates,
-        'cates': cates,
-        'side_bars': side_bars,
-        'recently_comments': recently_comments,
-        'recently_posts': recently_posts,
-    }
-    return context
+        extra_context = {
+            'nav_cates': nav_cates,
+            'cates': cates,
+            'side_bars': side_bars,
+            'recently_comments': recently_comments,
+            'recently_posts': recently_posts,
+        }
+        return super(CommonMixin, self).get_context_data(**extra_context)
 
 
-def post_list(request, category_id=None, tag_id=None):
-    queryset = Post.objects.all()
+class BasePostsView(CommonMixin, ListView):
+    model = Post
+    template_name = 'blog/list.html'
+    context_object_name = 'posts'
+    paginate_by = 2
 
-    if category_id:
-        queryset = Post.objects.filter(category_id=category_id)
-    elif tag_id:
+
+class IndexView(BasePostsView):
+    pass
+
+
+class CategoryView(BasePostsView):
+    def get_queryset(self):
+        qs = super(CategoryView, self).get_queryset()
+        cate_id = self.kwargs.get('category_id')
+        qs = qs.filter(category_id=cate_id)
+        return qs
+
+
+class TagView(BasePostsView):
+    def get_queryset(self):
+        tag_id = self.kwargs('tag_id')
         try:
             tag = Tag.objects.get(id=tag_id)
         except Tag.DoesNotExist:
-            queryset = []
-        else:
-            queryset = tag.posts.all()
+            return []
 
-    page = request.GET.get('page', 1)
-    try:
-        page = int(page)
-    except TypeError:
-        page = 1
-
-    page_size = 2
-    paginator = Paginator(queryset, page_size)
-    try:
-        posts = paginator.page(page)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    context = {
-        'posts': posts,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-    return render(request, 'blog/list.html', context=context)
+        posts = tag.posts.all()
+        return posts
 
 
-def post_detail(request, pk=None):
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
-    context = {
-        'post': post,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-    return render(request, 'blog/detail.html', context=context)
+class AuthorView(BasePostsView):
+    def get_queryset(self):
+        qs = super(AuthorView, self).get_queryset()
+        author_id = self.request.GET.get('author_id')
+        if author_id:
+            qs = qs.filter(owner_id=author_id)
+        return qs
+
+
+class PostView(CommonMixin, DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
