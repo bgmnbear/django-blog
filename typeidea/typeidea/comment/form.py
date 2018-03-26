3adcb37ad2f7b901029c27ef454bcb5c202410ef
unknown <jasonwhister@gmail.com>
2018-03-26 17:36:44 +0800

    使用 form 实现 target 提交

--- typeidea/typeidea/comment/form.py
+++ typeidea/typeidea/comment/form.py
@@ -8,6 +8,13 @@ from comment.models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    target = forms.CharField(max_length=100, widget=forms.widgets.HiddenInput)
+    content = forms.CharField(
+        label="内容",
+        max_length=100,
+        widget=forms.widgets.Textarea(attrs={'rows': 6, 'cols': 80, })
+    )
+
     def clean_content(self):
         content = self.cleaned_data.get('content')
         if len(content) < 10:
@@ -16,4 +23,4 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['nickname', 'email', 'website', 'content']
+        fields = ['target', 'nickname', 'email', 'website', 'content']
